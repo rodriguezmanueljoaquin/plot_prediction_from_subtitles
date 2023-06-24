e7f88eb378c29033214664c280ff914dfd8a6610
manurodriguez <manurodriguez@itba.edu.ar>
2023-06-23 21:12:39 -0300

    :recycle: code dir

--- code/train.py
+++ code/train.py
@@ -13,7 +13,7 @@ import evaluate
 import numpy as np
 
 generator_model_name = "google/long-t5-tglobal-large"
-df = pd.read_csv('./data/dataset_True_True_True_True_True.csv')
+df = pd.read_csv('../data/dataset_True_True_True_True_True.csv')
 test_split = 0.3
 
 movies = Dataset.from_pandas(df)
@@ -90,4 +90,4 @@ trainer = Seq2SeqTrainer(
 
 trainer.train()
 
-trainer.save_model("movie-overview-predictor")
+trainer.save_model("../model-backup/movie-overview-predictor")
--- data/main.py
+++ data/main.py
@@ -1,10 +1,10 @@
-from data.generate_dataset import generate_dataset
+from generate_dataset import generate_dataset
 from analysis import analysis
 
 if __name__ == '__main__':
     data_df = generate_dataset(to_lower_case=True, remove_contractions=True, remove_stopwords=True,\
              remove_symbols=True, apply_lemmatization=True)
     
-    data_df.to_csv('./data/dataset_True_True_True_True_True.csv', index=False)
+    data_df.to_csv('./dataset_True_True_True_True_True.csv', index=False)
 
     analysis(data_df)
