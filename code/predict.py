5c9005a617cf7256e41565ba7eec25a5c185537b
Francisco Quesada <fquesada@itba.edu.ar>
2023-06-25 03:18:44 +0000

    :sparkles: add predict file and improve training

---
+++ code/predict.py
@@ -0,0 +1,55 @@
+import sys
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSeq2SeqLM,
+    DataCollatorForSeq2Seq,
+    Seq2SeqTrainingArguments,
+    Seq2SeqTrainer,
+)
+import pandas as pd
+from datasets import DatasetDict, Dataset
+import evaluate
+import numpy as np
+import os
+
+df = pd.read_csv('/opt/movies/data/dataset_True_True_True_True_True.csv')
+
+movies = Dataset.from_pandas(df)
+train_test = movies.train_test_split(shuffle = True, test_size=0.3, seed=1)
+df = train_test["test"].to_pandas()
+
+def test_model(run_path, metric='rouge'):
+  generator_run_path = run_path# "t5-base"#"../runs/t5-base/model"
+  tokenizer = AutoTokenizer.from_pretrained(f'{generator_run_path}/model')
+  model = AutoModelForSeq2SeqLM.from_pretrained(f'{generator_run_path}/model', return_dict=True).to('cuda')
+
+  def evaluate_fun():
+    predictions = []
+    targets = []
+    for serie in df.itertuples():
+      text = f'summarize: {serie.overview}'
+      inputs = tokenizer.encode(text, return_tensors="pt", truncation=False).to('cuda')
+      outputs = model.generate(inputs, do_sample=False, num_beams=3, max_length=10, early_stopping=True)
+      output = tokenizer.decode(outputs[0], skip_special_tokens=True)
+      # print(f'expected: {serie.title} - predicted: {output}')
+      predictions.append(output)
+      targets.append(serie.title)
+    
+    return predictions, targets
+
+  predictions, targets = evaluate_fun()
+
+  # save csv with predictions and targets
+  test_df = pd.DataFrame({'predictions': predictions, 'targets': targets})
+  test_df.to_csv(f'{run_path}/test.csv', index=False)
+
+  metrics = evaluate.load(metric)
+  results = metrics.compute(predictions=predictions, references=targets, use_stemmer=True)
+  # save results
+  with open(f'{run_path}/results.txt', 'w') as f:
+    f.write(str(results))
+  
+if __name__ == '__main__':
+  # iterate through all files in runs folder
+  for run_path in os.listdir('/opt/movies/runs'):
+    test_model(f'/opt/movies/runs/{run_path}')
--- code/train.py
+++ code/train.py
@@ -5,96 +5,100 @@ from transformers import (
     DataCollatorForSeq2Seq,
     Seq2SeqTrainingArguments,
     Seq2SeqTrainer,
+    TrainerCallback
 )
 import pandas as pd
 from datasets import DatasetDict, Dataset
 import evaluate
 import numpy as np
+from transformers.integrations import TensorBoardCallback
+import os
 
-generator_model_name = "t5-base"
-df = pd.read_csv('/opt/movies/data/dataset_True_True_True_True_True.csv')
+def train(model_name, dataset_path, test_split=0.3, learning_rate=2e-5, epochs=10):
 
-test_split = 0.3
+  generator_model_name = model_name
+  df = pd.read_csv(dataset_path)
+  test_split = test_split
 
-movies = Dataset.from_pandas(df)
+  movies = Dataset.from_pandas(df)
 
-tokenizer = AutoTokenizer.from_pretrained(generator_model_name)
+  tokenizer = AutoTokenizer.from_pretrained(generator_model_name)
 
-def preprocess(examples):
-    t5_task_prefix = "summarize: " 
-    inputs = [t5_task_prefix + doc for doc in examples["overview"]]
-    model_inputs = tokenizer(inputs, truncation=True)
-    labels = tokenizer(examples["title"], truncation=True, max_length=10)
+  def preprocess(examples):
+      t5_task_prefix = "summarize: " 
+      inputs = [t5_task_prefix + doc for doc in examples["overview"]]
+      model_inputs = tokenizer(inputs, truncation=True)
+      labels = tokenizer(examples["title"], truncation=True, max_length=10)
 
-    model_inputs["labels"] = labels["input_ids"]
-    return model_inputs
+      model_inputs["labels"] = labels["input_ids"]
+      return model_inputs
 
-tokenized_subtitles = movies.map(preprocess, batched=True)
-train_test = tokenized_subtitles.train_test_split(shuffle = True, test_size=test_split, seed=1)
-# test_valid = train_testvalid["test"].train_test_split(shuffle = True, test_size=(test_split/(test_split + val_split)), seed=random_state)
+  tokenized_overviews = movies.map(preprocess, batched=True)
+  train_test = tokenized_overviews.train_test_split(shuffle = True, test_size=test_split, seed=1)
+  # test_valid = train_testvalid["test"].train_test_split(shuffle = True, test_size=(test_split/(test_split + val_split)), seed=random_state)
 
-model = AutoModelForSeq2SeqLM.from_pretrained(generator_model_name)
-data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=generator_model_name)
+  model = AutoModelForSeq2SeqLM.from_pretrained(generator_model_name)
+  data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=generator_model_name)
 
-tokenized_subtitles = DatasetDict(
-    train = train_test["train"],
-    test = train_test["test"],
-)
-
-
-metrics = evaluate.load("rouge")
-
-def compute_metrics(eval_pred):
-    print("COMPUTE METRICS")
-    predictions, labels = eval_pred
-    decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True, batch_size=1)
-    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
-    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True, batch_size=1)
-
-    result = metrics.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
-
-    prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in predictions]
-    result["gen_len"] = np.mean(prediction_lens)
-    print("COMPUTE METRICS END")
-    return {k: round(v, 4) for k, v in result.items()}
+  tokenized_overviews = DatasetDict(
+      train = train_test["train"],
+      test = train_test["test"],
+  )
 
 
-training_args = Seq2SeqTrainingArguments(
-    output_dir="movie-overview-predictor",
-    evaluation_strategy="epoch",
-    learning_rate=1e-6,
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
-    weight_decay=0.01,
-    save_total_limit=3,
-    num_train_epochs=3000,
-    predict_with_generate=True,
-    fp16=True,
-    push_to_hub=False
-)
+  metrics = evaluate.load("rouge")
 
-trainer = Seq2SeqTrainer(
-    model=model,
-    args=training_args,
-    train_dataset=tokenized_subtitles["train"],
-    eval_dataset=tokenized_subtitles["test"],
-    tokenizer=tokenizer,
-    data_collator=data_collator,
-    compute_metrics=compute_metrics
-)
+  def compute_metrics(eval_pred):
+      predictions, labels = eval_pred
+      decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True, batch_size=1)
+      labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+      decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True, batch_size=1)
 
-trainer.train()
+      result = metrics.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
 
-trainer.save_model("/opt/movies/model-backup/movie-overview-predictor")
+      prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in predictions]
+      result["gen_len"] = np.mean(prediction_lens)
+      return {k: round(v, 4) for k, v in result.items()}
 
-print("LO DEJE CORRIENDO A LAS 3:10")
+  output_dir = f"/opt/movies/runs/{model_name}_lr{learning_rate}_epochs{epochs}"
 
-# subtitle = df_total['overview'][20]
-# print(len(subtitle))
-# text = "summarize: " + subtitle
-# inputs = tokenizer.encode(text, return_tensors="pt", truncation=False).to("cuda")
-# print(inputs)
-# outputs = model.generate(inputs, do_sample=False, num_beams=3)
-# output = tokenizer.decode(outputs[0], skip_special_tokens=True)
-# print("EXPECTED " + subtitle)
-# print("PREDICTED " + output)
+  training_args = Seq2SeqTrainingArguments(
+      output_dir=f"{output_dir}/train-results",
+      evaluation_strategy="steps",
+      learning_rate=learning_rate,
+      per_device_train_batch_size=48,
+      per_device_eval_batch_size=48,
+      weight_decay=0.01,
+      save_total_limit=3,
+      logging_steps=50,
+      logging_dir=f"{output_dir}/logs",
+      logging_strategy="steps",
+      num_train_epochs=epochs,
+      predict_with_generate=True,
+      fp16=True,
+      push_to_hub=False,
+      do_eval=True,
+      eval_steps=50,
+  )
+
+  trainer = Seq2SeqTrainer(
+      model=model,
+      args=training_args,
+      train_dataset=tokenized_overviews["train"],
+      eval_dataset=tokenized_overviews["test"],
+      tokenizer=tokenizer,
+      data_collator=data_collator,
+      compute_metrics=compute_metrics,
+  )
+
+  trainer.train()
+
+  trainer.save_model(f"{output_dir}/model")
+
+if __name__ == "__main__":
+  model_name = "facebook/bart-base"#"t5-base"
+  dataset_path = "/opt/movies/data/dataset_True_True_True_True_True.csv"
+  test_split = 0.3
+  learning_rate = 2e-5
+  epochs = 2
+  train(model_name, dataset_path, test_split, learning_rate, epochs)
